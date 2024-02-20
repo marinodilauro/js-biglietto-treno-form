2424002ee197f36e563ebe34ec320f33d44b105e
marinodilauro <m.dilauro90@gmail.com>
2024-02-20 16:10:35 +0100

    Add a button to reset the input field and re-run the application

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,6 @@ let userName = inputNameElement.value;
 
 const inputAgeElement = document.getElementById("inputAgeRange");
 let userAge = inputAgeElement.value;
-console.log(userAge);
 
 const inputDistanceElement = document.getElementById("inputDistance");
 let travelDistance = inputDistanceElement.value;
@@ -26,11 +25,13 @@ console.log(ticketFinalPrice);
 const minorDiscount = 0.2;
 const seniorDiscount = 0.4;
 
-// Stampo il prezzo del biglietto
+// Stampo il prezzo del biglietto al click del bottone "Scopri il prezzo"
 
 const submitButtonElement = document.getElementById("submitBtn");
 
 submitButtonElement.addEventListener("click", function () {
+
+  // Assegno alle variabili il valore degli inputs dell'utente
 
   userName = inputNameElement.value;
   console.log(userName);
@@ -53,7 +54,39 @@ submitButtonElement.addEventListener("click", function () {
     ticketFinalPrice = ticketFinalPrice - (ticketFinalPrice * seniorDiscount);
   }
 
+  // Stampo il prezzo del biglietto nella pagina
+
   let priceOutput = document.getElementById("price");
 
   priceOutput.innerHTML = `Il biglietto costerà ${ticketFinalPrice.toLocaleString("it-IT", { style: "currency", currency: "EUR" })}`;
+});
+
+
+
+// Resetto i campi e riavvio l'app al click del bottone "Ripeti"
+
+const repeatButtonElement = document.getElementById("repeatBtn");
+
+repeatButtonElement.addEventListener("click", function () {
+
+  // Resetto il valore degli inputs dell'utente
+
+  inputAgeElement.value = "";
+  console.log(userName);
+
+  inputNameElement.value = "";
+  console.log(userAge);
+
+  inputDistanceElement.value = "";
+  console.log(travelDistance);
+
+  // Resetto il valore del prezzo del biglietto
+
+  let ticketFinalPrice = null;
+
+  // Cancello il prezzo del biglieto dalla pagina
+
+  let priceOutput = document.getElementById("price");
+
+  priceOutput.innerHTML = null;
 });
