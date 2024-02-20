8092b7552346d989e8dbd2a1571d4993ad9c5570
marinodilauro <m.dilauro90@gmail.com>
2024-02-20 15:05:14 +0100

    Add js variables for user name, age and travel distance

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,11 @@
 // Creo due variabili per l'età dell'utente e i km che deve percorrere
 // Assegno a queste due variabili dei valori tramite input utente
 
+const inputNameElement = document.getElementById("inputName");
+console.log(inputNameElement.value);
+
+const inputAgeElement = document.getElementById("inputAge");
+console.log(inputAgeElement.value);
+
+const inputDistanceElement = document.getElementById("inputDistance");
+console.log(inputDistanceElement.value);
